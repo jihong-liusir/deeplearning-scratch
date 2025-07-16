e97a6df5d6ff881a3ac45b5e5593fed552a9259e
jihong-liusir <liusir_hi@hotmail.com>
2025-07-16 23:26:00 +0800

    NAND, OR

--- CH2/perceptron.py
+++ CH2/perceptron.py
@@ -1,6 +1,6 @@
 import numpy as np
 
-def AND1(x1, x2):
+def AND1(x1, x2): #与门
     w1, w2, theta = 0.5, 0.5, 0.7
     temp = x1*w1 + x2*w2
     if temp <= theta:
@@ -8,7 +8,7 @@ def AND1(x1, x2):
     elif temp > theta:
         return 1
 
-def AND(x1, x2):
+def AND(x1, x2): # 与门
     x = np.array([x1, x2])
     w = np.array([0.5, 0.5])
     b = -0.7
@@ -18,9 +18,25 @@ def AND(x1, x2):
     else:
         return 1
 
-def NAND(x1, x2):
-    return 0
+def NAND(x1, x2): # 与非门
+    x = np.array([x1, x2])
+    w = np.array([-0.5, -0.5]) # 仅权重和偏置与AND不同
+    b = 0.7
+    tmp = np.sum(w*x) + b
+    if tmp <= 0:
+        return 0
+    else:
+        return 1
 
+def OR(x1, x2): # 或门
+    x = np.array([x1, x2])
+    w = np.array([0.5,0.5])
+    b = -0.2
+    tmp = np.sum(w*x) + b
+    if tmp <= 0:
+        return 0
+    else:
+        return 1
 
 print(AND(0,0))
 print(AND(0,1))
